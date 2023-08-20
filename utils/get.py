f1bfaa66a9ff42cc38c8a19ff76cb85f36d8e3bf
Aditya Chaturvedi <aditya.chaturvedi1608@gmail.com>
2023-08-21 00:42:26 +0530

    Format

--- utils/get.py
+++ utils/get.py
@@ -9,6 +9,7 @@ All images have creative common license.
 
 import requests
 
+
 def get_images(author: str):
     """
     Get images of the author
@@ -20,20 +21,20 @@ def get_images(author: str):
     """
 
     # Openverse API
-    IMAGES_URL = f'https://api.openverse.engineering/v1/images/?q={author}'
+    IMAGES_URL = f"https://api.openverse.engineering/v1/images/?q={author}"
 
     res = requests.get(IMAGES_URL)
-    images = res.json()['results']
+    images = res.json()["results"]
 
     return images
+
 
 def get_quote():
     """
     Get random quotes
     """
-    API_URL = 'https://zenquotes.io/api/quotes'
+    API_URL = "https://zenquotes.io/api/quotes"
     res = requests.get(API_URL)
-
 
     quote = res.json()
 
--- utils/image.py
+++ utils/image.py
@@ -14,6 +14,7 @@ from tempfile import gettempdir
 from time import gmtime, strftime
 from PIL import Image, ImageFont, ImageDraw
 
+
 def create_image(quote: str, image_url: str, color: tuple) -> None:
     """
     Create the quote image
@@ -23,22 +24,21 @@ def create_image(quote: str, image_url: str, color: tuple) -> None:
 
     quote: str
     image_url: str
-    color: tuple 
+    color: tuple
     """
 
     TIMESTAMP = strftime("%H:%M:%S", gmtime())
-    OUTPUT_DIR = 'output' # Path where generated images are stored
+    OUTPUT_DIR = "output"  # Path where generated images are stored
 
     # Temporary image. Stored in temporary directory
-    T_IMG_NAME = path.join(gettempdir(),
-                       f'qimage-{TIMESTAMP}.jpg')
+    T_IMG_NAME = path.join(gettempdir(), f"qimage-{TIMESTAMP}.jpg")
 
     # Image attributes
     WIDTH = 1080
     HEIGHT = 1350
 
     # Create new image
-    img = Image.new(mode='RGB', size = (WIDTH, HEIGHT), color=color)
+    img = Image.new(mode="RGB", size=(WIDTH, HEIGHT), color=color)
 
     # Download the author image
     urllib.request.urlretrieve(image_url, T_IMG_NAME)
@@ -47,23 +47,27 @@ def create_image(quote: str, image_url: str, color: tuple) -> None:
     qimg = Image.open(T_IMG_NAME)
     qimg = qimg.resize((1080, 877))
 
-    mask = Image.open('assets/mask.png')
+    mask = Image.open("assets/mask.png")
 
     # Convert the image to RGBA so that we can composite the mask and the image
-    qimg = qimg.convert('RGBA')
+    qimg = qimg.convert("RGBA")
     qimg = Image.alpha_composite(qimg, mask)
 
     # Add the image
     img.paste(qimg, (0, 0))
 
     # Load fonts
-    afont = ImageFont.truetype('fonts/Montserrat-Bold.ttf', 50)
-    qfont = ImageFont.truetype('fonts/Montserrat-Regular.ttf', 58)
+    afont = ImageFont.truetype("fonts/Montserrat-Bold.ttf", 50)
+    qfont = ImageFont.truetype("fonts/Montserrat-Regular.ttf", 58)
 
-    ImageDraw.fontmode = 'L' # Enable anti-aliasing for fonts
+    ImageDraw.fontmode = "L"  # Enable anti-aliasing for fonts
 
-    ImageDraw.Draw(img).text((382, 719), quote['a'], (0, 0, 0), font=afont) # Add author
-    ImageDraw.Draw(img).text((131, 833), textwrap.fill(quote['q'], 30), (0, 0, 0), font=qfont) # Add quote
+    ImageDraw.Draw(img).text(
+        (382, 719), quote["a"], (0, 0, 0), font=afont
+    )  # Add author
+    ImageDraw.Draw(img).text(
+        (131, 833), textwrap.fill(quote["q"], 30), (0, 0, 0), font=qfont
+    )  # Add quote
 
     # Save the final image
 
